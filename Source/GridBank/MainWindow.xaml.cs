7f3a25adef1c2fbf525822d86f73985b08a73225
Walt Daniels <WaltDaniels@gmail.com>
2015-04-10 15:21:27 -0400

    Wire-up Gauge to buttons

--- Source/GridBank/MainWindow.xaml.cs
+++ Source/GridBank/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MahApps.Metro.Controls;
+using System.ComponentModel;
 
 namespace GridBank
 {
@@ -7,19 +8,73 @@ namespace GridBank
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private MainWindowViewModel viewModel;
+
+        private double _clickIncrement = .025;
+
         public MainWindow()
         {
             InitializeComponent();
+            viewModel = new MainWindowViewModel();
+            DataContext = viewModel;
         }
 
         private void Charge_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-
+            if (viewModel.CurrentValue <= (1 - _clickIncrement))
+            {
+                viewModel.CurrentValue = viewModel.CurrentValue + .025;
+            }
+            else
+            {
+                viewModel.CurrentValue = 1;
+            }
         }
 
         private void Discharge_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            if (viewModel.CurrentValue >= _clickIncrement)
+            {
+                viewModel.CurrentValue = viewModel.CurrentValue - .025;
+            }
+            else
+            {
+                viewModel.CurrentValue = 0;
+            }
+        }
+    }
 
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private double _currentValue;
+
+        public MainWindowViewModel()
+        {
+            CurrentValue = .65;
+        }
+
+        public double CurrentValue
+        {
+
+            get { return _currentValue; }
+            set
+            {
+                if (value == _currentValue)
+                    return;
+
+                _currentValue = value;
+                OnPropertyChanged("CurrentValue");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
