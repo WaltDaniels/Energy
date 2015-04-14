b8f3f1f3e3ebaaa461f4992a2e38f69ced665b85
Walt Daniels <WaltDaniels@gmail.com>
2015-04-14 17:21:15 -0400

    CentralHub - Collect Data wired up to GridBankApi

---
+++ Source/CentralHub/Controllers/GridBankController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using CentralHubService;
+
+namespace CentralHub.Controllers
+{
+    public class GridBankController : Controller
+    {
+        public async Task<ActionResult> Collect()
+        {
+            var service = new CollectUsage();
+            
+            var usageDetails = await service.GetData(1);
+            service.SaveData(usageDetails);
+
+            return View(usageDetails);
+        }
+
+        public ActionResult Display()
+        {
+            return View();
+        }
+    }
+}
--- Source/CentralHubService/CollectUsage.cs
+++ Source/CentralHubService/CollectUsage.cs
@@ -1,21 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 using CentralHubData;
 using CentralHubService.Models;
-using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace CentralHubService
 {
     public class CollectUsage
     {
-        public async void GetData(int SiteId)
+        public async Task<Details> GetData(int SiteId)
         {
+            Details retDetails = null;
+
             //Lookup Site's URL
             using (var context = new CentralHubDb())
             {
@@ -26,49 +25,64 @@ namespace CentralHubService
                 DateTime lastUpdate = lastUsage != null ? lastUsage.TimeStamp : new DateTime();
 
                 var uri = string.Format(
-                    "{0}?siteid={1}&detailsStartingDateTime={2}", 
+                    "{0}/api/usage/getupates?siteid={1}&detailsStartingDateTime={2}", 
                     site.ApiUrl, 
                     site.Id,
                     WebUtility.UrlEncode(lastUpdate.ToString()));
 
+                //Make the WebAPI call to collect data
                 var httpClient = new HttpClient();
-
-                //string content = HttpGet(uri).Wait();
-                var content = await RestCall(uri);
-
-                //JObject content = httpClient.GetAsync(uri).Result;
-
-
-                //return await Task.Run(() => UsageModel.Details.Parse(content));
-
-            }
-
-
-            //Hit Site's WebAPI
-
-            //Return Data
-        }
-
-        private async Task<string> RestCall(string uri)
-        {
-            string payload = "";
-
-            using (var httpClient = new HttpClient())
-            {
-                //httpClient.BaseAddress = new Uri(uri);
-                httpClient.DefaultRequestHeaders.Accept.Clear();
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
                 HttpResponseMessage response = await httpClient.GetAsync(uri);
-                if (response.IsSuccessStatusCode)
+                if (response != null && response.StatusCode == HttpStatusCode.OK)
                 {
-                    payload = await response.Content.ReadAsStringAsync();
-                    //Product product = await response.Content.ReadAsAsync<Product>();
-                    //Console.WriteLine("{0}\t${1}\t{2}", product.Name, product.Price, product.Category);
+                    string jsonSting = await response.Content.ReadAsStringAsync();
+                    retDetails = JsonConvert.DeserializeObject<Details>(jsonSting);
+                }
+                else
+                {
+                    throw new Exception("Bad response from GridBankApi: " +
+                                        (response != null ? response.ReasonPhrase : "<unknown>"));
                 }
             }
 
-            return payload;
+            return retDetails;
+        }
+
+        public void SaveData(Details details)
+        {
+            using (var context = new CentralHubDb())
+            {
+                var site = context.GridBankSites.Find(details.SiteId);
+                if (site == null) throw new Exception("Site not found!");
+
+                foreach (var entry in details.UsageEntries)
+                {
+                    //See if this Guid already exists
+                    var currentRecord = site.Usages.FirstOrDefault(x => x.IdGuid == entry.IdGuid && x.GridBankSiteId == details.SiteId);
+                    if (currentRecord != null)
+                    {
+                        //Yes, it exists ... update it
+                        currentRecord.TimeStamp = entry.TimeStamp;
+                        currentRecord.CurrentPower = entry.CurrentPower;
+                        currentRecord.IsNew = true;
+                    }
+                    else
+                    {
+                        //No, this is a new entry ... add it
+                        site.Usages.Add(new Usage()
+                        {
+                            IdGuid = entry.IdGuid,
+                            GridBankSiteId = details.SiteId,
+                            TimeStamp = entry.TimeStamp,
+                            CurrentPower = entry.CurrentPower,
+                            IsNew = true
+                        });
+                    }
+                }
+
+                //Save everything
+                context.SaveChanges();
+            }
         }
     }
 }
